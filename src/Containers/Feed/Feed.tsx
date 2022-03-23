86f95bf2972df551e134d84ba3592a60c2ef052f
Hassan Masood <hassanmasood2002@gmail.com>
2022-03-23 21:54:37 +0500

    Responsiveness added

--- src/Containers/Feed/Feed.tsx
+++ src/Containers/Feed/Feed.tsx
@@ -8,44 +8,41 @@ import Search from "../../Components/Search";
 
 const Feed: FunctionComponent = () => {
   return (
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-xl-3 order-2 order-lg-1">
-              <Search />
-              <AboutProfile />
-              <Following />             
-            </div>
-
-
-            <div className="col-lg-8 col-xl-6 order-1 order-lg-2 mt-2 mb-3 pb-3">
-              <Post />
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <ImagePost />
-              </div>
-              <div>
-                <ImagePost />
-              </div> 
-            </div>
-
-            <div className="col-lg-12 col-xl-3 order-3 order-lg-3">
-              <Activity />
-      
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 col-xl-3 order-2 order-lg-1">
+          <Search />
+          <AboutProfile />
+          <Following />
+        </div>
+        <div className="col-lg-8 col-xl-6 order-1 order-lg-2 mt-2 mb-3 pb-3">
+          <Post />
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <ImagePost />
+          </div>
+          <div>
+            <ImagePost />
           </div>
         </div>
-  )
-}
+
+        <div className="col-lg-12 col-xl-3 order-3 order-lg-3">
+          <Activity />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Feed;
