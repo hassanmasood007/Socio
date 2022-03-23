c12267ef9873119f51a146fe793ae00c874075d8
Hassan Masood <hassanmasood2002@gmail.com>
2022-03-23 21:30:23 +0500

    responsiveness added

--- src/Containers/Feed/Feed.tsx
+++ src/Containers/Feed/Feed.tsx
@@ -4,48 +4,44 @@ import ImagePost from "../../Components/Post/ImagePost";
 import Activity from "../../Components/Profile/Activities";
 import AboutProfile from "../../Components/Profile/ProfileCard/AboutProfile";
 import Following from "../../Components/Profile/ProfileCard/Following";
-import Search from "../../Components/Search";
 
 const Feed: FunctionComponent = () => {
   return (
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-xl-3 order-2 order-lg-1">
-              <Search />
-              <AboutProfile />
-              <Following />             
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 col-xl-3 order-2 order-lg-1 mt-2">
+          <AboutProfile />
+          <Following />
+        </div>
 
-
-            <div className="col-lg-8 col-xl-6 order-1 order-lg-2 mt-2 mb-3 pb-3">
-              <Post />
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <Post />
-              </div>
-              <div>
-                <ImagePost />
-              </div>
-              <div>
-                <ImagePost />
-              </div> 
-            </div>
-
-            <div className="col-lg-12 col-xl-3 order-3 order-lg-3">
-              <Activity />
-      
-            </div>
+        <div className="col-lg-8 col-xl-6 order-1 order-lg-2 mt-2 mb-3 pb-3">
+          <Post />
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <Post />
+          </div>
+          <div>
+            <ImagePost />
+          </div>
+          <div>
+            <ImagePost />
           </div>
         </div>
-  )
-}
+
+        <div className="col-lg-12 col-xl-3 order-3 order-lg-3 ">
+          <Activity />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Feed;
